c32ee12f19e24d33d632ab7d93886bc9417ea96d
duangsuse <fedora-opensuse@outlook.com>
2017-11-13 21:43:44 +0800

    Add Icons&Themes

---
+++ app/src/main/java/org/duangsuse/tree/Green.java
@@ -0,0 +1,8 @@
+package org.duangsuse.tree;
+
+/**
+ * Created by dse on 17-11-13.
+ */
+
+public class Green extends MainActivity {
+}
---
+++ app/src/main/java/org/duangsuse/tree/Red.java
@@ -0,0 +1,8 @@
+package org.duangsuse.tree;
+
+/**
+ * Created by dse on 17-11-13.
+ */
+
+public class Red extends MainActivity {
+}
